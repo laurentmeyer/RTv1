26e37d6418bbb6e76cb2400445dec44320d63600
lmeyer <lmeyer@student.42.fr>
2019-01-19 13:01:23 +0100

    fix directional light shadows

--- srcs/render/shading.c
+++ srcs/render/shading.c
@@ -6,7 +6,7 @@
 /*   By: jpriou <jpriou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/18 12:09:47 by jpriou            #+#    #+#             */
-/*   Updated: 2019/01/19 12:08:46 by jpriou           ###   ########.fr       */
+/*   Updated: 2019/01/18 12:43:35 by jpriou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,32 @@
 #include "raytracer.h"
 #include "color.h"
 #include <math.h>
+#include <float.h>
 
 void		point_to_light(
-				t_v3 *direction,
+				t_hit	*out,
 				double *intensity,
 				t_light *light,
 				t_v3 *point)
 {
 	t_v3	vec;
 
+	ft_bzero(out, sizeof(t_hit));
+	out->ray.origin = *point;
 	*intensity = light->intensity;
 	if (light->type == DIRECTIONAL)
-		*direction = inverse(light->direction);
+	{
+		out->ray.direction = inverse(light->direction);
+		out->ray.max_length = DBL_MAX;
+	}
 	else if (light->type == SPOT)
 	{
 		vec = sub_v3(light->position, *point);
-		*direction = normalize(vec);
+		out->ray.direction = normalize(vec);
+        out->ray.max_length =
+                    magnitude(sub_v3(*point, light->position));
 		*intensity = *intensity / pow(magnitude(vec), 0.4);
-	}
+        }
 }
 
 t_color		shade_one_light(t_ram *ram, t_hit *hit, t_ray *ray, t_light *light)
@@ -40,25 +48,24 @@ t_color		shade_one_light(t_ram *ram, t_hit *hit, t_ray *ray, t_light *light)
 	t_color	color;
 	double	diffuse;
 	double	specular;
-	t_ray	l;
+	t_hit	l;
 	double	intensity;
 
-	point_to_light(&(l.direction), &intensity, light, &(hit->point));
-	l.origin = hit->point;
-	l.max_length = magnitude(sub_v3(hit->point, light->position));
+	point_to_light(&l, &intensity, light, &(hit->point));
 	color = (t_color){0., 0., 0.};
-	if (closest_intersection(ram->scene, &l, NULL))
+	if (closest_intersection(ram->scene, &(l.ray), NULL))
 		return (color);
-	diffuse = dot_product(l.direction, hit->normal);
+	diffuse = dot_product(l.ray.direction, hit->normal);
 	if (diffuse > 0.)
 		color = mul_v3(hit->object->material.color,
 					intensity * diffuse * hit->object->material.diffuse);
 	specular = dot_product(hit->normal,
-					normalize(add_v3(inverse(ray->direction), l.direction)));
+			normalize(add_v3(inverse(ray->direction), l.ray.direction)));
 	if (specular <= 0.)
 		return (color);
 	else
-		specular = pow(specular, hit->object->material.phong_exponent) *
+		specular = pow(specular,
+			hit->object->material.phong_exponent) *
 			hit->object->material.specular;
 	return (add_color(color,
 					mul_v3((t_color){1., 1., 1.}, intensity * specular)));
@@ -68,7 +75,7 @@ void		shade_pixel(t_ram *ram, t_hit *hit, t_ray *ray, t_color *out)
 {
 	size_t i;
 
-	*out = (t_v3){0., 0., 0.};
+	*out = mul_v3(hit->object->material.color, ram->scene->ambiant_light);
 	i = 0;
 	while (i < ram->scene->lights_count)
 		*out = add_color(*out,
